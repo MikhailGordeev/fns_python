bad5e77bb0d95d0dd75e157ab45a487ee1f7d74b
Mikhail Gordeev <gordeev.mv@gmail.com>
2018-03-22 23:41:59 +0300

    qr scanner function

---
+++ qrscanner.py
@@ -0,0 +1,11 @@
+from PIL import Image
+import zbarlight
+
+
+def get_qr_data(image_file):
+    file_path = image_file
+    with open(file_path, 'rb') as image_file:
+        image = Image.open(image_file)
+        image.load()
+    codes = zbarlight.scan_codes('qrcode', image)
+    print(codes)
