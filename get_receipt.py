006718af314a3e819173436ba1cf5ba3711cb0dc
Mikhail Gordeev <gordeev.mv@gmail.com>
2018-03-24 15:53:30 +0300

    release

--- get_receipt.py
+++ get_receipt.py
@@ -1,10 +1,10 @@
 import uuid
 from qrscanner import get_qr_data
 from furl import furl
-
+import requests
 
 # Random device ID
-random_uuid = str(uuid.uuid4()).replace('-', '')
+dev_id = str(uuid.uuid4()).replace('-', '')
 # DeviceID
 dev_os = 'Adnroid 4.4.4'
 # Protocol version
@@ -15,9 +15,33 @@ client = '1.4.1.3'
 uagent = 'okhttp/3.0.1'
 # Base URL
 base = 'https://proverkacheka.nalog.ru:9999'
+# Receipt QR code
+qr_file = 'qr1.png'
 
-f = furl('/?{}'.format(get_qr_data('qr.png')))
+f = furl('/?{}'.format(get_qr_data(qr_file)[0].decode("utf-8")))
+
+# Fiscal storage (Номер фискального накопителя - ФН)
 fn = f.args['fn']
+# Fiscal document number (Номер фискального документа - ФД)
 fd = f.args['i']
-fs = f.args['fp']
+# Fiscal sign (Подпись фискального документа - ФП)
+fp = f.args['fp']
 
+headers = {
+    'Device-Id': dev_id,
+    'Device-OS': dev_os,
+    'Version': proto,
+    'ClientVersion': client,
+    'User-Agent': uagent
+}
+
+data_request = [
+  ('fiscalSign', fp),
+  ('sendToEmail', 'no'),
+]
+
+request_receipt = "%s/v1/inns/*/kkts/*/fss/%s/tickets/%s" % (base, fn, fd)
+
+response = requests.get(request_receipt, headers=headers, params=data_request)
+
+print(response.json())
--- qrscanner.py
+++ qrscanner.py
@@ -8,4 +8,4 @@ def get_qr_data(image_file):
         image = Image.open(image_file)
         image.load()
     codes = zbarlight.scan_codes('qrcode', image)
-    return codes[0].decode("utf-8")
+    return codes
